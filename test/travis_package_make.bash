d8f68c7c083d8710f78f4af55e1f6b7fcef8dd4e
Tomonori Mase <mase.tomonori@jp.fujitsu.com>
2019-02-18 18:19:57 +0900

    new file:   .travis.yml 	new file:   launch/wall_stop.launch 	new file:   test/travis_package_make.bash 	new file:   test/travis_prepare_dummy_files.bash 	new file:   test/travis_ros_install.bash

---
+++ test/travis_package_make.bash
@@ -0,0 +1,13 @@
+#!/bin/bash -xve
+
+#sync
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+#get pimouse_ros
+cd ~/catkin_ws/src/
+git clone --depth=1 https://github.com/masetomonori/raspimouse_ros.git
+
+#make
+cd ~/catkin_ws
+catkin_make
+
