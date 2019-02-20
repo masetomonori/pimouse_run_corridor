04403bce99a9c2f9b18d069dfa749b94780dc0c3
Tomonori Mase <mase.tomonori@jp.fujitsu.com>
2019-02-20 18:20:16 +0900

    new file:   scripts/wall_stop.py 	modified:   test/travis_test_wall_stop.py

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -4,11 +4,28 @@ import rosnode, rospy
 import time
 
 class WallStopTest(unittest.TestCase):
-    def set_andget(self, lf, ls, rs, rf):
-        pass
+    def set_and_get(self, lf, ls, rs, rf):
+        with open("/dev/rtlightsensor0", "w") as f:
+            f.write("%d %d %d %d \n" % (rf, rs, ls, lf))
+
+            time.sleep(0.3)
+
+        with open("/dev/rtmotor_raw_l0", "r") as lf, \
+             open("/dev/rtmotor_raw_r0", "r") as rf:
+            left  = int(lf.readline().rstrip())
+            right = int(rf.readline().rstrip())
+
+        return left, right
 
     def test_io(self):
-       pass
+        left, right = self.set_and_get(400, 100, 100, 88) #total :600
+        self.assertTrue(left == 0 and right == 0, "can't stop")
+
+        left, right = self.set_and_get(400, 0, 0, 99) #total :499
+        self.assertTrue(left != 0 and right != 0, "can't move again")
+
+        left, right = self.set_and_get(150, 0, 200, 150) #total :500
+        self.assertTrue(left == 0 and right == 0, "can't stop")
 
 if __name__ == '__main__':
     time.sleep(3)
