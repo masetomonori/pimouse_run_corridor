edbd5bd831a84fba69506afe00c4bab48bb98877
Tomonori Mase <mase.tomonori@jp.fujitsu.com>
2019-02-20 12:19:53 +0900

    modified:   package.xml 	modified:   test/travis_package_make.bash

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -5,8 +5,7 @@ rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 
 #get pimouse_ros
 cd ~/catkin_ws/src/
-git clone --depth=1 https://github.com/masetomonori/raspimouse_ros.git
-
+git clone --depth=1 https://github.com/masetomonori/pimouse_ros.git
 #make
 cd ~/catkin_ws
 catkin_make
